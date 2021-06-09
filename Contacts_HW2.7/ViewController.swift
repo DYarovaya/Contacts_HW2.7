2a838cf2ec6a5799c24d26fd9ba4a008cdb2e3d5
Darya Yarovaya <yarovayadarya@mail.ru>
2021-06-09 10:55:05 +0300

    Initial Commit

---
+++ Contacts_HW2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts_HW2.7
+//
+//  Created by Дарья Яровая on 09.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
